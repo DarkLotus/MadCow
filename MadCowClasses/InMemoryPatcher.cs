d7ea2a0efad82107fb587041c228e604374727e4
James Kidd <kklolk@gmail.com>
2012-03-27 14:38:21 +1100

    Fixed second run.

--- MadCowClasses/InMemoryPatcher.cs
+++ MadCowClasses/InMemoryPatcher.cs
@@ -44,7 +44,7 @@ namespace MadCow.MadCowClasses
         {
             // Start a new thread, needed so we can wait for D3 to load battle.net.dll.
             if (_workerThread == null) { _workerThread = new Thread(new ParameterizedThreadStart(_patchThread)); _workerThread.Start(Diablo3); return; }
-            if (_workerThread.ThreadState != System.Threading.ThreadState.Running) { _workerThread.Start(Diablo3); }
+            if (_workerThread.ThreadState != System.Threading.ThreadState.Running) { _workerThread = new Thread(new ParameterizedThreadStart(_patchThread)); _workerThread.Start(Diablo3); }
         }
 
         internal static void _patchThread(object obj)
